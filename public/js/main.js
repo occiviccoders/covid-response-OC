93d05d7891e28c73f309bbb881f318c7541bee61
Andrew J Akagawa <andrewakagawa@Andrews-Mac-mini.local>
2020-03-21 11:00:54 -0700

    adding chart

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,34 @@
 // note data are from data.js... Im not proud of that, but was quick to hack together
 // use cvoc for data object
+// charts
+let ctx = document.getElementById('chart').getContext('2d');  // for the chart
+let chart = null;
+
+// parse the chart data
+cvoc.chartTotals = function(){
+    return cvoc.counts.reduce(function(returnArray, datum){
+        returnArray.labels.push(datum.label);
+        returnArray.counts.push(Number(datum.data.find(
+            function(criteria){
+                return criteria.category==="Total Cases" && criteria.type==="Cases";
+            }
+        ).count));
+        return returnArray;
+    },{labels:[], counts:[]});
+}
+
+// calculate numbers
+const totals = cvoc.chartTotals();
+
+// load the chart
+cvoc.chart = new Chart (ctx, {
+    type: 'bar',
+    data: {
+        labels: totals.labels,
+        datasets: [{
+            label:  'Total Cases',
+            data: totals.counts,
+        }]
+    }
+});
+
