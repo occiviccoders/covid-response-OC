39d018eabf2954c529c22a7aa2f4df7013a7eeba
Andrew J Akagawa <andrewakagawa@Andrews-Mac-mini.local>
2020-03-21 14:35:41 -0700

    adding how section

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
 // note data are from data.js... Im not proud of that, but was quick to hack together
 // use cvoc for data object
 // charts
-let ctx_totals = document.getElementById('chart_totals').getContext('2d');  // for the chart
+const ctx_totals = document.getElementById('chart_totals').getContext('2d');  // for the chart
+const ctx_age_by_time = document.getElementById('chart_age_by_time').getContext('2d');  // for the chart
 
 // gets counts by category and type
 cvoc.getCounts = function(category, type, date){
@@ -20,43 +21,61 @@ cvoc.chartTotals = function(){
     return cvoc.counts.reduce(function(returnArray, date){
         returnArray.labels.push(date.label);
         returnArray.datasets[0].data.push(cvoc.getCounts("Total Cases", "Cases", date));
-        returnArray.datasets[1].data.push(cvoc.getCounts("Total Deaths", "Cases", date));
+        returnArray.datasets[1].data.push(cvoc.getCounts("Total Cases", "Total Deaths", date));
         return returnArray;
     },{
         labels:[],
         datasets: [{
             label:  'Total Cases',
             data: [],
-            backgroundColor: 'rgba(255, 99, 132, 0.3)',
-            borderColors: 'rgba(255, 99, 132, 1)',
+            backgroundColor: 'rgba(198, 91, 16, 0.5)',
+            borderColor: 'rgba(198, 91, 16, 1)',
         },{
             label:  'Total Dealths',
             data: [],
             backgroundColor: 'rgb(155, 155, 155, 0.3)',
-            borderColors: 'rgb(155, 155, 155, 1)',
+            borderColor: 'rgb(155, 155, 155, 1)',
             type: 'line',
         }],
     });
 }
 
-// parse the count data into chart catagorys data
-cvoc.chartByTypes = function(){
-    return cvoc.counts.reduce(function(returnArray, datum){
-        returnArray.labels.push(datum.label);
-        returnArray.datasets[0].data.push(Number(datum.data.find(
-            function(criteria){
-                return criteria.category==="Total Cases" && criteria.type==="Cases";
-            }
-        ).count));
+// parse the count data into chart categories data
+cvoc.ageByTime = function(){
+    return cvoc.counts.reduce(function(returnArray, date){
+        returnArray.labels.push(date.label);
+        returnArray.datasets[0].data.push(cvoc.getCounts("Total Cases", "Travel Related", date));
+        returnArray.datasets[1].data.push(cvoc.getCounts("Total Cases", "Person to Person Spread*", date));
+        returnArray.datasets[2].data.push(cvoc.getCounts("Total Cases", "Community Acquired**", date));
+        returnArray.datasets[3].data.push(cvoc.getCounts("Total Cases", "Under Investigation", date));
         return returnArray;
     },{
         labels:[],
         datasets: [{
-            label:  'Total Cases',
+            label:  'Travel Related',
+            fill: false,
             data: [],
-            backgroundColor: 'rgba(255, 99, 132, 0.3)',
-            borderColors: 'rgba(255, 99, 132, 1)',
-        }]
+            backgroundColor: 'rgba(6, 58, 110, 1)',
+            borderColor: 'rgba(6, 58, 110, 1)',
+        },{
+            label:  'Person to Person Spread*',
+            fill: false,
+            data: [],
+            backgroundColor: 'rgba(6, 110, 83, 1)',
+            borderColor: 'rgba(6, 110, 83, 1)',
+        },{
+            label:  'Community Acquired**',
+            fill: false,
+            data: [],
+            backgroundColor: 'rgba(183, 122, 10, 1)',
+            borderColor: 'rgba(183, 122, 10, 1)',
+        },{
+            label:  'Under Investigation',
+            fill: false,
+            data: [],
+            backgroundColor: 'rgba(135, 135, 135, 1)',
+            borderColor: 'rgba(135, 135, 135, 1)',
+        }],
     });
 }
 
@@ -67,11 +86,22 @@ cvoc.chart_totals = new Chart (ctx_totals, {
     options: {
         tooltips: {
             mode: 'index'
+        },
+        legend: {
+            position: 'bottom'
         }
     }
 });
-/*cvoc.chart_types= new Chart (ctx_types, {
-    type: 'bar',
-    data: cvoc.chartTotals(),
-});*/
+cvoc.chart_age_by_time= new Chart (ctx_age_by_time, {
+    type: 'line',
+    data: cvoc.ageByTime(),
+    options: {
+        tooltips: {
+            mode: 'index'
+        },
+        legend: {
+            position: 'bottom'
+        }
+    }
+});
 
