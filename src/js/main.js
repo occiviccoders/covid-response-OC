081705d8929d6169132fab62fd54175ff45a6da7
Andrew J Akagawa <andrewakagawa@Andrews-Mac-mini.local>
2020-03-21 12:58:49 -0700

    adding data labels and death count

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,77 @@
 // note data are from data.js... Im not proud of that, but was quick to hack together
 // use cvoc for data object
 // charts
-let ctx = document.getElementById('chart').getContext('2d');  // for the chart
-let chart = null;
+let ctx_totals = document.getElementById('chart_totals').getContext('2d');  // for the chart
 
-// parse the chart data
+// gets counts by category and type
+cvoc.getCounts = function(category, type, date){
+    const datum = date.data.find(function(criteria){
+        return criteria.category===category && criteria.type===type;
+    });
+    if (datum && datum.count) {
+        return Number(datum.count);
+    } else {
+        return 0;
+    }
+}
+
+// parse the count data into chart totals data
 cvoc.chartTotals = function(){
+    return cvoc.counts.reduce(function(returnArray, date){
+        returnArray.labels.push(date.label);
+        returnArray.datasets[0].data.push(cvoc.getCounts("Total Cases", "Cases", date));
+        returnArray.datasets[1].data.push(cvoc.getCounts("Total Deaths", "Cases", date));
+        return returnArray;
+    },{
+        labels:[],
+        datasets: [{
+            label:  'Total Cases',
+            data: [],
+            backgroundColor: 'rgba(255, 99, 132, 0.3)',
+            borderColors: 'rgba(255, 99, 132, 1)',
+        },{
+            label:  'Total Dealths',
+            data: [],
+            backgroundColor: 'rgb(155, 155, 155, 0.3)',
+            borderColors: 'rgb(155, 155, 155, 1)',
+            type: 'line',
+        }],
+    });
+}
+
+// parse the count data into chart catagorys data
+cvoc.chartByTypes = function(){
     return cvoc.counts.reduce(function(returnArray, datum){
         returnArray.labels.push(datum.label);
-        returnArray.counts.push(Number(datum.data.find(
+        returnArray.datasets[0].data.push(Number(datum.data.find(
             function(criteria){
                 return criteria.category==="Total Cases" && criteria.type==="Cases";
             }
         ).count));
         return returnArray;
-    },{labels:[], counts:[]});
-}
-
-// calculate numbers
-const totals = cvoc.chartTotals();
-
-// load the chart
-cvoc.chart = new Chart (ctx, {
-    type: 'bar',
-    data: {
-        labels: totals.labels,
+    },{
+        labels:[],
         datasets: [{
             label:  'Total Cases',
-            data: totals.counts,
+            data: [],
+            backgroundColor: 'rgba(255, 99, 132, 0.3)',
+            borderColors: 'rgba(255, 99, 132, 1)',
         }]
+    });
+}
+
+// load the charts
+cvoc.chart_totals = new Chart (ctx_totals, {
+    type: 'bar',
+    data: cvoc.chartTotals(),
+    options: {
+        tooltips: {
+            mode: 'index'
+        }
     }
 });
+/*cvoc.chart_types= new Chart (ctx_types, {
+    type: 'bar',
+    data: cvoc.chartTotals(),
+});*/
 
